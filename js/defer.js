5d1209d76866dc6bcae3b2b74f50443624e487cb
danilosheen <c.danilo.f.silva@gmail.com>
2024-03-13 23:03:25 -0300

    feat: section sobre

--- js/defer.js
+++ js/defer.js
@@ -1,8 +1,3 @@
 document.getElementById("theme-toggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
-  // Verifica se o tema atual é dark ou light e altera o link da folha de estilo correspondente
-  var theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
-  document
-    .getElementById("theme")
-    .setAttribute("href", "./css/style-" + theme + ".css");
 });
