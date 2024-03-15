573a5186bda289bc96117f248480acd510a0a8ff
danilosheen <c.danilo.f.silva@gmail.com>
2024-03-15 10:21:46 -0300

    chore: atualização de responsividade e atualizar código para test

--- js/defer.js
+++ js/defer.js
@@ -1,3 +1,19 @@
 document.getElementById("theme-toggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
 });
+
+window.addEventListener("scroll", function () {
+  let sections = document.querySelectorAll("section");
+  sections.forEach(function (section) {
+    let rect = section.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      let id = section.getAttribute("id");
+      document.querySelectorAll(".box-nav-item").forEach(function (item) {
+        item.classList.remove("active");
+      });
+      document
+        .querySelector('a[href="#' + id + '"]')
+        .parentNode.parentNode.classList.add("active");
+    }
+  });
+});
