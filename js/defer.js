7a9831113499b90a5a26db4a7ad376daf82ef4ae
danilosheen <c.danilo.f.silva@gmail.com>
2024-03-18 15:37:46 -0300

    feat: projetos e rodapé

--- js/defer.js
+++ js/defer.js
@@ -1,19 +1,18 @@
 document.getElementById("theme-toggle").addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
+  const rodape = document.getElementById("rodape-grad");
+  const name = document.querySelector(".name-span");
+  rodape.classList.toggle("gradient2");
+  rodape.classList.toggle("gradient");
+  name.classList.toggle("gradient2");
+  name.classList.toggle("gradient");
 });
 
-window.addEventListener("scroll", function () {
-  let sections = document.querySelectorAll("section");
-  sections.forEach(function (section) {
-    let rect = section.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-      let id = section.getAttribute("id");
-      document.querySelectorAll(".box-nav-item").forEach(function (item) {
-        item.classList.remove("active");
-      });
-      document
-        .querySelector('a[href="#' + id + '"]')
-        .parentNode.parentNode.classList.add("active");
-    }
-  });
-});
+const btnMobile = document.getElementById("btn-mobile");
+
+function toggleMenu() {
+  const nav = document.getElementById("navbar-bs");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
