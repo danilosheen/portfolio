524ae06b6a98601e43ac4d1d1575725a0bb04240
danilosheen <c.danilo.f.silva@gmail.com>
2024-03-28 12:27:02 -0300

    fix: function load and set gradient

--- js/defer.js
+++ js/defer.js
@@ -1,26 +1,40 @@
 // botão alteração de tema light-dark
 document.getElementById("theme-toggle").addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
-  const rodape = document.getElementById("rodape-grad");
-  const name = document.querySelector(".name-span");
-  rodape.classList.toggle("gradient2");
-  rodape.classList.toggle("gradient");
-  name.classList.toggle("gradient2");
-  name.classList.toggle("gradient");
-
   const isDarkTheme = document.body.classList.contains("dark-mode");
   localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+
+  aplicarCores();
 });
+
+//Verificar tema e aplicar cores
+function aplicarCores() {
+  const isDarkTheme = document.body.classList.contains("dark-mode");
+  const rodape = document.getElementById("rodape-grad");
+  const name = document.querySelector(".name-span");
+
+  name.classList.replace(
+    name.classList[3],
+    ("theme", isDarkTheme ? "gradient" : "gradient2")
+  );
+
+  rodape.classList.replace(
+    rodape.classList[1],
+    isDarkTheme ? "gradient" : "gradient2"
+  );
+}
 
 // Verificar e aplicar o tema atual ao carregar a página
 window.onload = () => {
   const savedTheme = localStorage.getItem("theme");
   const body = document.body;
+
   if (savedTheme === "dark") {
     body.classList.add("dark-mode");
   } else {
     body.classList.remove("dark-mode");
   }
+  aplicarCores();
 };
 
 // active navbar mobile
