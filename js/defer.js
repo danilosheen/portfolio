f3f21f0253dd7755daf4cbb014a88e32bb635375
danilosheen <c.danilo.f.silva@gmail.com>
2024-04-05 11:52:58 -0300

    fix: span gradient

--- js/defer.js
+++ js/defer.js
@@ -1,17 +1,28 @@
 // botão alteração de tema light-dark
 document.getElementById("theme-toggle").addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
-  const isDarkTheme = document.body.classList.contains("dark-mode");
-  localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
-
+  saveTheme();
   aplicarCores();
 });
+
+function saveTheme() {
+  const isDarkTheme = document.body.classList.contains("dark-mode");
+  localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+}
 
 //Verificar tema e aplicar cores
 function aplicarCores() {
   const isDarkTheme = document.body.classList.contains("dark-mode");
   const rodape = document.getElementById("rodape-grad");
   const name = document.querySelector(".name-span");
+  const span = document.querySelectorAll(".span-size");
+
+  span.forEach((element) => {
+    element.classList.replace(
+      element.classList[1],
+      ("theme", isDarkTheme ? "gradient" : "gradient2")
+    );
+  });
 
   name.classList.replace(
     name.classList[3],
